0054a071d72c16de4731838daa4c6f8a00068f42
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-04-27 18:02:27 -0300

    Req 3

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -25,18 +25,13 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    return [job for job in jobs if job["job_type"] == job_type]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    # usei como base para resolução deste requisito
+    # os exercicios do course, em req 4, solução,
+    # aproximadamente na linha 12 a 15
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    #  for book in books:
+    #         for category in book["categories"]:
+    #             if not categories.get(category):
+    #                 categories[category] = 0
