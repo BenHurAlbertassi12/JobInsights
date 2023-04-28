f9c8268a4833a3ee043ccaaffa5a70a57a3615e5
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-04-28 01:00:46 -0300

    Req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    for job in read_brazilian_file("tests/mocks/brazilians_jobs.csv"):
+        keys = set()
+        for key in job.keys():
+            keys.add(key)
+            # adiciona as key ao conjunto keys
+
+    assert keys == {"title", "salary", "type"}
+    # assert para comparar os resultados
